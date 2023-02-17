a87a8c6d396cc1f1dc4268837e56f4241dd2faf4
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-17 10:06:39 -0800

    LowerCase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints an alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		putchar(sl);
+	}
+		putchar('\n');
+	return (0);
+}
+
