6c37867a7b9d6527678a1ff34c05254178bb7491
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-17 14:44:58 -0800

    All Combos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combinations of digits
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
