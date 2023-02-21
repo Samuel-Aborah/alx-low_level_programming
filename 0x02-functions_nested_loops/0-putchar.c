84950501c7d7af318ca17451a474c0bc091c4db1
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-21 06:17:32 -0800

    0-Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - A program that uses  putchar to print
+ *'_putchar' followed by a new line.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This to use functions and nested loops to solve the project
