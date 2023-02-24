bb5bd3bbb581159f96fcfa391ef0a60b08f98b0d
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-24 06:07:48 -0800

    Most Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * print_most_numbers - Prints numbers from 0 to 9 except 2 and 4
- * followed by a new line.
  *
+ *Return: void
  */
 
 void print_most_numbers(void)
 {
-	char Bb;
+	int Bb;
 
-	for (Bb = 0; Bb <= 9; Bb++)
+	for (Bb = '0'; Bb <= '9'; Bb++)
 	{
-		if (Bb != 2 && Bb != 4)
+		if (Bb != '2' && Bb != '4')
 
 			_putchar(Bb);
 	}
