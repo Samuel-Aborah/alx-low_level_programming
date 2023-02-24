547b9a10e47719d60f681b3eba3e454317719466
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-24 13:50:28 -0800

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,20 +4,20 @@
  * print_triangle - prints a triangle, followed by a new line
  *@size: Size of the triangle
  *
+ *Return: void
  */
 
 void print_triangle(int size)
 {
+	
+	int m, p, q;
+	
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int m;
-		int p;
-		int q;
-
 		for (m = 1; m <= size; m++)
 		{
 			for (q = size - m; q >= 1; q--)
