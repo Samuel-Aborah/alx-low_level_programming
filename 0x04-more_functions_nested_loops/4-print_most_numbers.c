d0b80f921eaa3c51f535a6e261f70363fb875d39
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-24 05:27:30 -0800

    Number Exceptions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9 except 2 and 4
+ * followed by a new line.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int Bb;
+
+	for (Bb = 0; Bb <= 9; Bb++)
+	{
+		if (Bb != 2 && Bb != 4)
+
+			_putchar(Bb);
+	}
+		_putchar('\n');
+}
