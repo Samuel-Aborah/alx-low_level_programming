8356b4f60fc76455d18d62adf283443cd6a5d51d
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-02-24 06:52:34 -0800

    Draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: the number of times the character '-' should be printed
+ *
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int m;
+
+		for (m = 1; m <= n; m++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
