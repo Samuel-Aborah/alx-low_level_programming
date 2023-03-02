752b512d74559d754f2c65e053950641371fef15
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-02 12:21:11 -0800

    String cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - Concatenates two strings
+ * @dest: the destination string
+ * @src: the source strings
+ *Return: string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2;
+	int i;
+
+	for (i < 0; i <= len2; i++)
+	{
+		dest[len1 + 1] = src[i];
+			i++;
+
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This a project on Strings pointers and arrays and how these are coded using c programming
