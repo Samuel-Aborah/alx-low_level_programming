3bcb8a9581c3118b1634855f9fb56cde7b969940
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-02 15:25:32 -0800

    Alpha

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters to uppercase strings
+ * @i: pointer
+ * Return: i
+ *
+ */
+
+char *string_toupper(char *i)
+{
+	int j = 0;
+
+	while (i[j] != '\0')
+	{
+		if (i[j] >= 'a' && i[j] <= 'z')
+			i[j] = i[j] - 32;
+		i++;
+
+	}
+	return (i);
+
+
+}
