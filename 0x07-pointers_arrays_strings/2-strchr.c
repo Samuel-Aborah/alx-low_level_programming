8f35f226a7ea46e9bd13be8500b32899cdfc0230
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-06 07:25:46 -0800

    Locate a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string input
+ * @c: The character input
+ * Return: 0 always
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+
+}
