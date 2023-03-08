b2628d8df78e4f35cb54cc21582ca245dbc854f8
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-08 15:04:43 -0800

    Recursive string print

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number
+ *@n: the given number
+ *
+ * Return: 0 always
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	return n * factorial(n = 1)
+}
+
+int main ()
+{
+	_putchar (n);
+
+}
+_putchar ('\n');
+return (0);
