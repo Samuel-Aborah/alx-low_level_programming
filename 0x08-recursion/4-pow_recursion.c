6939ceeacbae82b56acb40debfc90e58b8ace6ce
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-09 02:08:32 -0800

    x ^ y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  *  _pow_recursion - returns the value of x raised to the power of y.
  *  @x: integer
  *  @y: the power
- *  Return: 0 always
+ *  Return: pow recursion
  *
  */
 
@@ -13,7 +13,10 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 
-	else
+	else if (y >= 1)
 		return (x * _pow_recursion(x, y - 1));
+
+	else
+		return (1);
 
 }
