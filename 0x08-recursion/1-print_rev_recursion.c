b2452a24de8b068c9084cd599f8cdfa0fd767d0c
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-08 16:20:01 -0800

    Reverse string print

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-
-	else
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
-
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
 }
