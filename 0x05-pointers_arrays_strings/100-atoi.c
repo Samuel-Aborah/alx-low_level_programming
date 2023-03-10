f9f1cf56f31ed8c6e7d52511b2dbe9a380b5d47e
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-10 07:50:32 -0800

    Converts a string into an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _atoi - Converts astring to an integer
+ * _atoi - Converts a string to an integer
  * @s: String to be converted
  *
  * Return: Integer converted from string
@@ -10,43 +10,30 @@
 
 int _atoi(char *s)
 {
-	int i, b, a, l, e, c;
+	int c = 0;
+	unsigned int a = 0;
+	int b = 1;
+	int d = 0;
 
-	i = 0;
-	b = 0;
-	a = 0;
-	l = 0;
-	e = 0;
-	c = 0;
-
-	while (s[l] != '\0')
-		l++;
-
-	while (i < l && e == 0)
+	while (s[c])
 	{
-		if (s[i] == '-')
-			++b;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[c] == 45)
 		{
-			c = s[i] - '0';
-			if (b % 2)
-				c = -c;
-			a = a * 10 + c;
-			e = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			e = 0;
+			b *= -1;
 		}
-		i++;
+		if (s[c] >= 48 && s[c] <= 57)
+		{
+			d = 1;
+			a = (a * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (d == 1)
+		{
+			break;
+		}
+		c++;
 	}
-
-	if (e == 0)
-		return (0);
-
-		return (a);
-
-
-
-
+	a *= d;
+	return (a);
 }
