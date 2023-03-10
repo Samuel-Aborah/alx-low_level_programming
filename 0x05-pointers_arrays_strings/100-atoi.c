142183a4d7093dd386f15ed999f373b9cc4247be
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-10 08:25:16 -0800

    string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,29 +11,29 @@
 int _atoi(char *s)
 {
 	int c = 0;
-	unsigned int a = 0;
-	int b = 1;
-	int d = 0;
+	int b = -1;
+	int a;
 
-	while (s[c])
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		if (s[c] == 45)
-		{
+		if (s[a] == '-')
 			b *= -1;
-		}
-		if (s[c] >= 48 && s[c] <= 57)
-		{
-			d = 1;
-			a = (a * 10) + (s[c] - '0');
-			c++;
-		}
 
-		if (d == 1)
+		if (s[a] > 47 && s[a] < 58)
 		{
-			break;
+			if (c < 0)
+				c = (c * 10) - (s[a] - '0');
+			else
+				c = (s[a] - '0') * -1;
+
+			if (s[a + 1] < 48 || s[a + 1] > 57)
+				break;
+
 		}
-		c++;
 	}
-	a *= d;
-	return (a);
+	if (b < 0)
+		c *= -1;
+
+	return (c);
 }
+
