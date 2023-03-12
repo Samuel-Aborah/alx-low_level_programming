c22a6bd4575aa76b536dde6c29de3ec3b4d733a7
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-12 00:38:30 -0800

    Multiplication of a & b

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,23 +11,20 @@
 
 int main(int argc, char **argv)
 {
-	int num1; 
+	int num1;
 	int num2;
-	int product;
 
-	if (argc <= 2)
+
+	if (argc < 3)
 	{
 		printf("error\n");
 		return (1);
 	}
-	
-		product = (num1 * num2);
 
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-		printf("%d\n", product);
+		printf("%d\n", num1 * num2);
 
-	
+
 		return (0);
 }
-
