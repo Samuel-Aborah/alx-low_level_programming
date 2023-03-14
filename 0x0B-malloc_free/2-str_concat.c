59cdec94c78857a0d7e53cca294a97dd0a0f83ea
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-14 05:22:23 -0700

    Cat two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,23 +12,22 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int len1 = 0, len2 = 0;
+	int i = 0, j = 0;
+	int len1 = 0, len2 = 0, len;
 	char *result;
 
-	if (s1 == NULL)
-		s1 == "";
-
-	if (s2 == NULL)
-		s2 == "";
-
-	while (s1[len1] != '\0')
-		len1++;
-
-	while (s2[len2] != '\0')
-		len2++;
-
-	result = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+	if (s1 != NULL)
+	{
+		while (s1[i++] != '\0')
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[i++] != '\0')
+			len2++;
+	}
+	len = len1 + len2;
+	result = (char *) malloc((len + 1) * sizeof(char));
 
 	if (result == NULL)
 		return (NULL);
@@ -36,10 +35,10 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 
-	for (i = 0; i < len2; i++)
-		result[i] = s2[i];
+	for (j = 0; j < len2; j++)
+		result[i] = s2[j];
 
-	result[len1 + len2] = '\0';
+	result[len] = '\0';
 
 	return (result);
 }
