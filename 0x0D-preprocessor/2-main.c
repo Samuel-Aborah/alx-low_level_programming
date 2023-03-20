1e70a3968a6856677e3e7bcc0cdf907623922580
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-20 08:09:43 -0700

    Prints name of file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * it was compiled from
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf("%S\n", __FILE__);
+	return (0);
+}
