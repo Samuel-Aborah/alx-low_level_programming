667f74aea983d82571244780ab869a13222d83af
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-20 10:41:41 -0700

    Define Struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+0x0E. C - Structures, typedef
+
+A project on structures and using typedef to give a type, a new name
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#define struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
