16cbed11e743becd4748136677a32f1b778d6771
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-30 07:35:22 -0700

    Print all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,14 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-	if (h->str == NULL)
+		if (!h->str)
 
-		printf("[0] (nill)\n");
+			printf("[0] (nill)\n");
 
-	else
-		printf("[%u]%s\n", h->len, h->str);
-
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		node++;
