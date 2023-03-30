3c856ad4ac58e6d9b9dfef86152c0afe7bf28c7d
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-30 08:09:34 -0700

    Add a new node at the beginning of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 #include <string.h>
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * Add node - A function that adds a new node at the 
+ * add_node - A function that adds a new node at the
  * beginning of a list
  * @head: head of the lists
  * @str: pointer to the head of the lists
@@ -13,33 +12,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t node;
+	list_t *nodeA;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
 
 	/** Allocate memory for the node */
-	node = (lsit_t *) malloc(sizeof(list_t));
+	nodeA = (list_t *) malloc(sizeof(list_t));
 
-	if (node == NULL)
+	if (nodeA == NULL)
 		return (NULL);
 
 	/** duplicate str */
-	node->str = strdup(str);
+	nodeA->str = strdup(str);
 
-	if (node->str == NULL)
-	{
-		free (node);
-		return (NULL);
-	}
-	node->len = 0;
+	/** assign address values to nodes */
+	nodeA->len = len;
+	nodeA->next = *head;
+	*head = nodeA;
 
-	/** assign value to the node */
-	while (str[node->len] != '\0')
-		node->len++;
-
-	node-> next = NULL;
-
-	/** create a head */ 	
-	head = node;
-
-	return (node);
-
+	return (*head);
 }
