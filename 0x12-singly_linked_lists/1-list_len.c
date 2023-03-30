e44d4e7bde3e8e41ec3053d8fe0d96ed489cee27
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-03-30 07:55:23 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * list_len - A function that returns the number of
+ * list_len - A function that returns number of
  * elements in a linked list
- * @h: pointer to the head of the list
- * Return: The number of elements
+ * @h: pointer to the head of the lists
+ * Return: Returns the number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
 	while (h)
 	{
