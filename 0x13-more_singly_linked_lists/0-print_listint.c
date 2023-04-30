7a468e129ccfb68ca01df6a1e75a0bc342ddab71
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-04-30 12:14:48 -0700

    Print all elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - A function that prints all elements of a list
+ * @h: pointer to the head of a list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
