e8643ca61ca426b577b684696854af1423c8cb88
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-05-02 13:48:20 -0700

    Safely prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,15 +14,23 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *nxt;
 	size_t num = 0;
 
+	/* point head pointer to current node */
 	curr = head;
 
+	/* Initiate pointer to NULL */
+	nxt = NULL;
+
+	/* traverse the list */
 	for (; curr != NULL; num++)
 	{
+		/* print current's node address and value */
 		printf("[%p] %d\n", (void *)curr, curr->n);
 		nxt = curr;
+		/* move to next node */
 		curr = curr->next;
 
-		if (nxt <= curr)
+		/* check if the nxt pointer points to an already accessed node */
+		if (nxt != NULL && nxt <= curr)
 		{
 			printf("-> [%p] %d\n", (void *)curr, curr->n);
 			exit(98);
