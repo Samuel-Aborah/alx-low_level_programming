7e73c576026d96c27567be450b881573c3166b22
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-05-02 12:28:15 -0700

    Prints the safe version of a list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,41 +3,31 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - A function that prints a linked list
- * @head: Pointer to the head of a list
- * Return: number of nodes in list, else exit with status 98
- * upon failure
+ * print_listint_safe - prints the safe versio of a list
+ * @head: pointer to the head of the list
+ * Return: number of nodes to the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *curr, *new;
+	const listint_t *curr;
+	const listint_t *nxt;
 	size_t num = 0;
 
 	curr = head;
-	new = head->next;
 
-	/* Using a loop where curr moves by 1 and new by 2 */
-	while (new != NULL && new < curr)
+	for (; curr != NULL; num++)
 	{
 		printf("[%p] %d\n", (void *)curr, curr->n);
+		nxt = curr;
 		curr = curr->next;
-		new = new->next;
 
-		if (new != NULL && new < curr)
-			new = new->next;
-		num++;
+		if (nxt <= curr)
+		{
+			printf("-> [%p] %d\n", (void *)curr, curr->n);
+			exit(98);
+		}
+
 	}
-
-	/* new reaches end of list, prints addr & value of last node to curr */
-	if (new != NULL)
-	{
-		printf("[%p] %d\n", (void *)curr, curr->n);
-		num++;
-	}
-
-	if (!head)
-		exit(98);
-
 	return (num);
 }
