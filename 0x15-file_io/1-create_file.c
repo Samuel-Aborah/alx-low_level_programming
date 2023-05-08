998142ad080b0b03ac2ec496946607b2c869c7d6
Samuel Kusi-Aborah <samuelaborah08@gmail.com>
2023-05-08 11:20:54 -0700

    Create File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
 
 /**
  * create_file - A function that creates a file
- * @filename: Name of file to create 
+ * @filename: Name of file to create
  * @text_content: A null terminated string to write to the file
  * Return: 1 on success or -1 on failure
  */
@@ -14,8 +14,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_describe, length;
 
-	file_describe = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR);
-	
+	file_describe = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
+			S_IRUSR | S_IWUSR);
+
 	if (filename == NULL)
 		return (-1);
 
@@ -23,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		length = strlen(text_content);
 
-		if (write(file_describe, text_content, length)!= length)
+		if (write(file_describe, text_content, length) != length)
 		{
 			close(file_describe);
 			return (-1);
